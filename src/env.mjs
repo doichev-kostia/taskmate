d1c7a54a1a7d2467c777c976d357d0be1068b5fb
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-04-20 18:47:29 +0200

    feat: env vars

--- src/env.mjs
+++ src/env.mjs
@@ -5,8 +5,9 @@ import { z } from "zod";
  * built with invalid env vars.
  */
 const server = z.object({
-  DATABASE_URL: z.string().url(),
-  NODE_ENV: z.enum(["development", "test", "production"]),
+	DATABASE_URL: z.string().url(),
+	DIRECT_URL: z.string().url(),
+	NODE_ENV: z.enum(["development", "test", "production"])
 });
 
 /**
@@ -14,7 +15,7 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+	// NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
 });
 
 /**
@@ -24,9 +25,10 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
-  NODE_ENV: process.env.NODE_ENV,
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+	DATABASE_URL: process.env.DATABASE_URL,
+	DIRECT_URL: process.env.DIRECT_URL,
+	NODE_ENV: process.env.NODE_ENV
+	// NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
 // Don't touch the part below
@@ -41,40 +43,40 @@ const merged = server.merge(client);
 let env = /** @type {MergedOutput} */ (process.env);
 
 const skip =
-  !!process.env.SKIP_ENV_VALIDATION &&
-  process.env.SKIP_ENV_VALIDATION !== "false" &&
-  process.env.SKIP_ENV_VALIDATION !== "0";
-if (!skip) {
-  const isServer = typeof window === "undefined";
+	!!process.env.SKIP_ENV_VALIDATION &&
+	process.env.SKIP_ENV_VALIDATION !== "false" &&
+	process.env.SKIP_ENV_VALIDATION !== "0";
+if ( !skip ) {
+	const isServer = typeof window === "undefined";
 
-  const parsed = /** @type {MergedSafeParseReturn} */ (
-    isServer
-      ? merged.safeParse(processEnv) // on server we can validate all env vars
-      : client.safeParse(processEnv) // on client we can only validate the ones that are exposed
-  );
+	const parsed = /** @type {MergedSafeParseReturn} */ (
+		isServer
+			? merged.safeParse(processEnv) // on server we can validate all env vars
+			: client.safeParse(processEnv) // on client we can only validate the ones that are exposed
+	);
 
-  if (parsed.success === false) {
-    console.error(
-      "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors,
-    );
-    throw new Error("Invalid environment variables");
-  }
+	if ( parsed.success === false ) {
+		console.error(
+			"❌ Invalid environment variables:",
+			parsed.error.flatten().fieldErrors
+		);
+		throw new Error("Invalid environment variables");
+	}
 
-  env = new Proxy(parsed.data, {
-    get(target, prop) {
-      if (typeof prop !== "string") return undefined;
-      // Throw a descriptive error if a server-side env var is accessed on the client
-      // Otherwise it would just be returning `undefined` and be annoying to debug
-      if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
-        throw new Error(
-          process.env.NODE_ENV === "production"
-            ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
-        );
-      return target[/** @type {keyof typeof target} */ (prop)];
-    },
-  });
+	env = new Proxy(parsed.data, {
+		get( target, prop ) {
+			if ( typeof prop !== "string" ) return undefined;
+			// Throw a descriptive error if a server-side env var is accessed on the client
+			// Otherwise it would just be returning `undefined` and be annoying to debug
+			if ( !isServer && !prop.startsWith("NEXT_PUBLIC_") )
+				throw new Error(
+					process.env.NODE_ENV === "production"
+						? "❌ Attempted to access a server-side environment variable on the client"
+						: `❌ Attempted to access server-side environment variable '${ prop }' on the client`
+				);
+			return target[/** @type {keyof typeof target} */ (prop)];
+		}
+	});
 }
 
 export { env };
