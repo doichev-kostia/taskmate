21e6c73a1ed4a1a85b5d3213a4456f988e695f63
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-04-23 16:05:05 +0200

    feat: requirements for boards

---
+++ src/server/requirements/has-access-to-board.requirement.ts
@@ -0,0 +1,29 @@
+import { prisma } from "~/server/db";
+import { TRPCError } from "@trpc/server";
+
+type Params = {
+	userId: string;
+	boardId: string;
+};
+export const hasAccessToBoardRequirement = async ({
+	userId,
+	boardId,
+}: Params): Promise<void> => {
+	const board = await prisma.board.findFirst({
+		where: {
+			id: boardId,
+			members: {
+				some: {
+					id: userId,
+				},
+			},
+		},
+	});
+
+	if (!board) {
+		throw new TRPCError({
+			code: "FORBIDDEN",
+			message: "Forbidden",
+		});
+	}
+};
---
+++ src/server/requirements/has-board-edit-rights.requirement.ts
@@ -0,0 +1,32 @@
+import { prisma } from "~/server/db";
+import { TRPCError } from "@trpc/server";
+
+type Params = {
+	userId: string;
+	boardId: string;
+};
+export const hasBoardEditRightsRequirement = async ({
+	userId,
+	boardId,
+}: Params): Promise<void> => {
+	const board = await prisma.board.findFirst({
+		where: {
+			id: boardId,
+			members: {
+				some: {
+					id: userId,
+					role: {
+						in: ["ADMIN", "OWNER"],
+					},
+				},
+			},
+		},
+	});
+
+	if (!board) {
+		throw new TRPCError({
+			code: "FORBIDDEN",
+			message: "Forbidden",
+		});
+	}
+};
