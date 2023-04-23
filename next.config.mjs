515ec7b7b2b4199a43991f9b7713ccbdb3ee4b25
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-04-23 10:36:27 +0200

    feat: base path redirect

--- next.config.mjs
+++ next.config.mjs
@@ -6,20 +6,30 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  }
+	reactStrictMode: true,
+	experimental: {
+		appDir: true,
+		typedRoutes: true,
+	},
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/dashboard",
+				permanent: true
+			}
+		];
+	}
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  // i18n: {
-  //   locales: ["en"],
-  //   defaultLocale: "en",
-  // },
+	/**
+	 * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+	 * out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+// i18n: {
+//   locales: ["en"],
+//   defaultLocale: "en",
+// },
 };
 export default config;
