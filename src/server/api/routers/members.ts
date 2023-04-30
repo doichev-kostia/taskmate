a3e91df8a95a1fd2744d1f3a8c46c26d53426c63
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-04-30 15:38:33 +0200

    feat: representation

--- src/server/api/routers/members.ts
+++ src/server/api/routers/members.ts
@@ -1,31 +1,35 @@
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 import { MemberBodyValidator } from "~/contracts/member.body.validator";
 import { TRPCError } from "@trpc/server";
+import { MemberRepresentation } from "~/contracts/member.representation";
 
 export const memberRouter = createTRPCRouter({
-	getMember: privateProcedure.input(MemberBodyValidator).query(async function getMember({ ctx, input }) {
-		const { id, userId } = input;
+	getMember: privateProcedure
+		.input(MemberBodyValidator)
+		.output(MemberRepresentation)
+		.query(async function getMember({ ctx, input }) {
+			const { id, userId } = input;
 
-		const member = await ctx.prisma.member.findFirst({
-			where: {
-				OR: [
-					{
-						id,
-					},
-					{
-						userId,
-						boardId: input.boardId,
-					},
-				],
-			},
-		});
-
-		if (!member) {
-			throw new TRPCError({
-				code: "NOT_FOUND",
+			const member = await ctx.prisma.member.findFirst({
+				where: {
+					OR: [
+						{
+							id,
+						},
+						{
+							userId,
+							boardId: input.boardId,
+						},
+					],
+				},
 			});
-		}
 
-		return member;
-	}),
+			if (!member) {
+				throw new TRPCError({
+					code: "NOT_FOUND",
+				});
+			}
+
+			return member;
+		}),
 });
