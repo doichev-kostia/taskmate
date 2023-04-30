42d98602db9a8f24452fa495f8622c0d33f9391e
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-04-30 15:23:12 +0200

    fix: get member

--- src/server/api/routers/members.ts
+++ src/server/api/routers/members.ts
@@ -3,30 +3,29 @@ import { MemberBodyValidator } from "~/contracts/member.body.validator";
 import { TRPCError } from "@trpc/server";
 
 export const memberRouter = createTRPCRouter({
-	getMember: privateProcedure
-		.input(MemberBodyValidator)
-		.query(async function getMember({ ctx, input }) {
-			const { id, userId } = input;
+	getMember: privateProcedure.input(MemberBodyValidator).query(async function getMember({ ctx, input }) {
+		const { id, userId } = input;
 
-			const member = await ctx.prisma.member.findFirst({
-				where: {
-					OR: [
-						{
-							id,
-						},
-						{
-							userId,
-						},
-					],
-				},
+		const member = await ctx.prisma.member.findFirst({
+			where: {
+				OR: [
+					{
+						id,
+					},
+					{
+						userId,
+						boardId: input.boardId,
+					},
+				],
+			},
+		});
+
+		if (!member) {
+			throw new TRPCError({
+				code: "NOT_FOUND",
 			});
+		}
 
-			if (!member) {
-				throw new TRPCError({
-					code: "NOT_FOUND",
-				});
-			}
-
-			return member;
-		}),
+		return member;
+	}),
 });
