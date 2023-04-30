c49c59894dd4d629cb1e42db6437eac1d492d803
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-04-30 15:21:43 +0200

    feat: configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,10 @@ const config = {
 			}
 		],
 		"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-		"@typescript-eslint/no-misused-promises": "off"
+		"@typescript-eslint/no-misused-promises": "off",
+		"no-misused-promises": "off",
+		"@typescript-eslint/no-floating-promises": "off",
+		"no-floating-promises": "off"
 	}
 };
 
