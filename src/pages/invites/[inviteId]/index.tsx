16497e9e779e0f4228de7205b188a137e471a829
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-05-01 21:39:49 +0200

    feat: improved the error message for invite

--- src/pages/invites/[inviteId]/index.tsx
+++ src/pages/invites/[inviteId]/index.tsx
@@ -1,13 +1,6 @@
 import React, { useEffect } from "react";
 import { PrivateLayout } from "~/layouts/PrivateLayout";
-import {
-	Modal,
-	ModalBody,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	ModalOverlay,
-} from "@chakra-ui/modal";
+import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/modal";
 import { Button, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { api } from "~/utils/api";
@@ -18,22 +11,15 @@ import { isBrowser } from "~/utils/isBrowser";
 
 function AcceptInvitePage() {
 	const router = useRouter();
-	console.log({ query: router.query });
 	const res = z
 		.object({
 			inviteId: z.string().uuid(),
 		})
 		.safeParse(router.query);
 
-	const {
-		mutate: acceptInvite,
-		isLoading,
-		isSuccess,
-	} = api.invites.acceptInvite.useMutation();
+	const { mutate: acceptInvite, isLoading, isSuccess, error } = api.invites.acceptInvite.useMutation();
 
-	const searchParams = new URLSearchParams(
-		isBrowser() ? window.location.search : ""
-	);
+	const searchParams = new URLSearchParams(isBrowser() ? window.location.search : "");
 	const success = isSuccess || searchParams.get("success") === "true";
 
 	useEffect(() => {
@@ -45,6 +31,8 @@ function AcceptInvitePage() {
 			},
 		});
 	}, [res.success]);
+
+	console.dir({ error });
 
 	return (
 		<PrivateLayout>
@@ -65,32 +53,26 @@ function AcceptInvitePage() {
 						) : (
 							<>
 								<ModalHeader>
-									{success
-										? "Thank you for accepting the invite!"
-										: "Unexpected error!"}
+									{success ? "Thank you for accepting the invite!" : "Unexpected error!"}
 								</ModalHeader>
 								<ModalBody className="text-center">
 									{success ? (
-										<p>
-											You can go back to the dashboard and
-											see the board there.
-										</p>
+										<p>You can go back to the dashboard and see the board there.</p>
 									) : (
-										<div className="flex h-full items-center justify-center">
-											<CloseIcon
-												color="red"
-												className="h-[40px] w-[40px]"
-											/>
+										<div className="flex h-full flex-col items-center justify-center gap-3">
+											<CloseIcon color="red" className="block h-[40px] w-[40px]" />
+											{error?.data?.httpStatus === 404 && (
+												<p>
+													Invite not found. It is probably expired. Contact the board owner to
+													get another one.
+												</p>
+											)}
 										</div>
 									)}
 								</ModalBody>
 
 								<ModalFooter className="flex items-center justify-center">
-									<Button
-										colorScheme="brand"
-										as={Link}
-										href="/dashboard"
-									>
+									<Button colorScheme="brand" as={Link} href="/dashboard">
 										My dashboard
 									</Button>
 								</ModalFooter>
