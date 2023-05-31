ea6df7083be559432e06cf51a6f75ebed5f35265
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-05-31 06:58:09 +0200

    feat: constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,3 +8,14 @@ export const BOARD_IMAGES = [
 	"https://res.cloudinary.com/dtay106eo/image/upload/v1682436136/david-marcu-78A265wPiO4-unsplash_ifbuyf.webp",
 	"https://res.cloudinary.com/dtay106eo/image/upload/v1682436136/jay-mantri-TFyi0QOx08c-unsplash_ntnbzd.webp",
 ];
+
+export const ACCESS_TOKEN_KEY = "access_token";
+export const REDIRECT_KEY = "redirect_url";
+
+export const milliseconds = {
+	second: 1000,
+	minute: 60 * 1000,
+	hour: 60 * 60 * 1000,
+	day: 24 * 60 * 60 * 1000,
+	week: 7 * 24 * 60 * 60 * 1000,
+};
