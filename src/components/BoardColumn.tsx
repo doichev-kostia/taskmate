2ab3452c35200c84887d6cb79dbbe73cf72e5d6d
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-05-31 07:28:27 +0200

    feat: BoardColumn skeleton

--- src/components/BoardColumn.tsx
+++ src/components/BoardColumn.tsx
@@ -30,3 +30,21 @@ export const BoardColumn = ({ id, name, className, issues, openModal }: BoardCol
 		</div>
 	);
 };
+
+type BoardColumnSkeletonProps = {
+	className?: string;
+};
+
+export const BoardColumnSkeleton = ({ className }: BoardColumnSkeletonProps) => {
+	return (
+		<div className={cx("min-w-[200px] border-x border-solid border-slate-400 text-center", className)}>
+			<p className="animate-pulse border-b border-solid border-slate-500 py-4 text-slate-300">Status</p>
+			<div className="flex flex-col gap-3 p-2 ">
+				<div className="h-20 animate-pulse rounded-lg bg-slate-600" />
+				<div className="h-20 animate-pulse rounded-lg bg-slate-600" />
+				<div className="h-20 animate-pulse rounded-lg bg-slate-600" />
+				<div className="h-20 animate-pulse rounded-lg bg-slate-600" />
+			</div>
+		</div>
+	);
+};
