a165282dfaee4f89bedaca632b9ea3d4ce206a95
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-05-31 06:57:46 +0200

    feat: use custom solution instead of clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,25 @@
-import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { ACCESS_TOKEN_KEY } from "~/utils/constants";
 
 // Set the paths that don't require the user to be signed in
-const publicPaths = ["/sign-in*", "/sign-up*"];
+const publicPaths = ["/sign-in*"];
 const isApi = (path: string) => path.startsWith("/api");
 
 const isPublic = (path: string) => {
-	return publicPaths.find((x) =>
-		path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
-	);
+	return publicPaths.find((x) => path.match(new RegExp(`^${x}$`.replace("*$", "($|/)"))));
 };
 
 export default withClerkMiddleware((request: NextRequest) => {
 	if (isPublic(request.nextUrl.pathname) || isApi(request.nextUrl.pathname)) {
 		return NextResponse.next();
 	}
+
 	// if the user is not signed in redirect them to the sign in page.
-	const { userId } = getAuth(request);
+	const token = request.cookies.get(ACCESS_TOKEN_KEY);
 
-	if (!userId) {
-		// redirect the users to /pages/sign-in/[[...index]].ts
-
+	if (!token) {
 		const signInUrl = new URL("/sign-in", request.url);
 		signInUrl.searchParams.set("redirect_url", request.url);
 		return NextResponse.redirect(signInUrl);
